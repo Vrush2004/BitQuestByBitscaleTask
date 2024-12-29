46aa2d477fa6ca2fb007ce041c6c16f3289e9e06
Vrush2004 <vrushalichaube@gmail.com>
2024-12-29 21:08:11 +0530

    add column

---
+++ script.js
@@ -0,0 +1,10 @@
+function openModal() {
+    document.getElementById('modal-overlay').style.display = 'block';
+    document.getElementById('add-column-modal').style.display = 'block';
+}
+
+// Close the modal
+function closeModal() {
+    document.getElementById('modal-overlay').style.display = 'none';
+    document.getElementById('add-column-modal').style.display = 'none';
+}
